b008d3ae5ebd719d9769bbafbbbfe0e5ae413f32
Lakhwinder Singh Virk <boby_boby534@yahoo.com>
2021-05-31 10:44:37 -0500

    Add dotenv library to keep urls or any confidential data in that file.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ const alias = { '^@/(.+)': './src/\\1' }; // @/folder will be an alias to <root>
 const extensions = ['.android.js', '.ios.js', '.js', '.json', '.native'];
 
 const presets = ['module:metro-react-native-babel-preset'];
-const plugins = [['module-resolver', { alias, extensions }], 'react-native-reanimated/plugin'];
+const plugins = [
+  ['module-resolver', { alias, extensions }],
+  'react-native-reanimated/plugin',
+  [
+    'dotenv-import',
+    {
+      moduleName: '@env',
+      path: '.env',
+      blacklist: null,
+      whitelist: null,
+      safe: false,
+      allowUndefined: false,
+    },
+  ],
+];
 
 module.exports = { presets, plugins };
